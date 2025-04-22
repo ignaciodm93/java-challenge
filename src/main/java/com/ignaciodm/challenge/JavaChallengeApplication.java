0e8b3ff867a65cdf38ac7a4ac991838c1f5cf71b
NachoPC <ignaciodm93@gmail.com>
2025-04-22 13:41:15 -0300

    no message

--- src/main/java/com/ignaciodm/challenge/JavaChallengeApplication.java
+++ src/main/java/com/ignaciodm/challenge/JavaChallengeApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableReactiveMongoRepositories(basePackages = "com.ignaciodm.challenge.repository")
 public class JavaChallengeApplication {
 
 	public static void main(String[] args) {
